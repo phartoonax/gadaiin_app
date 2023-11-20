676c01bf46b78794230e50c14ccdbc2fdd5a5a83
Phartoonax <q7cpa8y1jqeluxxwjzi91p1rjcb9y2@bots.bitbucket.org>
2023-11-20 17:59:53 +0700

    push day finnished

--- src/components/botdrawerfilter.js
+++ src/components/botdrawerfilter.js
@@ -1,5 +1,15 @@
-import React from "react";
-import { Button, IconButton, Stack, SwipeableDrawer } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  SwipeableDrawer,
+} from "@mui/material";
+import dayjs from "dayjs";
+import { MobileDatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Icon } from "@iconify/react";
 
 const BotDrawerFilter = ({ open, setOpen }) => {
@@ -14,6 +24,14 @@ const BotDrawerFilter = ({ open, setOpen }) => {
     zIndex: 999,
     boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.1)",
   };
+
+  const [open2, setOpen2] = useState(false);
+  const [value, setValue] = useState(new dayjs("2022-04-17"));
+
+  const handleOpen = () => {
+    setOpen2(true);
+  };
+
   return (
     <>
       <SwipeableDrawer
@@ -21,29 +39,62 @@ const BotDrawerFilter = ({ open, setOpen }) => {
         open={open}
         onClose={() => setOpen(false)}
         PaperProps={{ style: drawerStyle }}
+        swipeAreaWidth={"38px"}
+        disableSwipeToOpen={true}
       >
         <div className="w-full pt-[5px] pb-[12px] flex-col justify-center items-center gap-[10px] inline-flex">
           <div className="w-[38px] h-[2px] bg-green-600 rounded-md"></div>
         </div>
         <Stack className="px-4" direction="row" justifyContent="space-between">
-          <IconButton onClick={() => setOpen(false)} style={{ width: "50px" }}>
+          <IconButton
+            onClick={() => setOpen(false)}
+            style={{ width: "62px", justifyContent: "flex-start" }}
+          >
             <Icon
               icon={"feather:x"}
               className="text-success-Main"
               style={{ fontSize: "24px" }}
             ></Icon>
           </IconButton>
-          <p className="text-base leading-[18px] font-bold text-success-Main text-center items-center flex ">
+          <div className="text-base leading-[18px] font-bold text-success-Main text-center items-center flex ">
             Filter
-          </p>
+          </div>
           <Button
             variant="text"
             className="text-success-Main font-semibold text-sm leading-[14px]"
             onClick={null}
-            style={{ width: "50px" }}
           >
             Reset
           </Button>
+        </Stack>
+        <Stack
+          direction="column"
+          divider={
+            <Divider variant="fullWidth" sx={{ marginY: "10px" }}></Divider>
+          }
+          sx={{ px: "16px" }}
+        >
+          <Stack gap={"10px"}>
+            <div>{"Tanggal Transaksi"}</div>
+            <Stack direction="row" justifyContent="space-between" width="100%">
+              <div onClick={handleOpen}>TEST</div>
+              <div>{"S/D"}</div>
+              <div>TES</div>
+            </Stack>
+          </Stack>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            {" "}
+            <MobileDatePicker
+              open={open2}
+              value={value}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+              onClose={() => setOpen2(false)}
+              slotProps={{ textField: { size: "small",  } }}
+            />
+          </LocalizationProvider>
+          <div>again</div>
         </Stack>
       </SwipeableDrawer>
     </>
